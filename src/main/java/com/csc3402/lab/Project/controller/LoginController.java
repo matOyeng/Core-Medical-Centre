a49ac14fda9f7d1660cf8d6e68a9f2db8d68fb22
matOyeng <118502233+matOyeng@users.noreply.github.com>
2024-06-27 17:40:50 +0800

    Okay

--- src/main/java/com/csc3402/lab/Project/controller/LoginController.java
+++ src/main/java/com/csc3402/lab/Project/controller/LoginController.java
@@ -1,4 +1,5 @@
 package com.csc3402.lab.Project.controller;
+
 import com.csc3402.lab.Project.dto.UserDto;
 import com.csc3402.lab.Project.model.User;
 import com.csc3402.lab.Project.service.UserService;
@@ -10,7 +11,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginController {
@@ -18,34 +18,40 @@ public class LoginController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping("/login")
-    public String LoginForm() {return "login";}
+    @GetMapping("/login")
+    public String loginForm() {
+        return "login";
+    }
 
-//    @GetMapping("/registration")
-//    public String registrationForm(Model model) {
-//        UserDto user = new UserDto();
-//        model.addAttribute("user", user);
-//        return "registration";
-//    }
-//    @PostMapping("/registration")
-//    public String registration (
-//            @Valid @ModelAttribute("user") UserDto userDto,
-//            BindingResult result,
-//            Model model) {
-//        User existingUser = userService.findUserByEmail(userDto.getEmail());
-//
-//        if (existingUser != null)
-//            result.rejectValue("email", null, "User already registered !!!");
-//
-//        if (result.hasErrors()) {
-//            model.addAttribute("user",userDto);
-//            return "/registration";
-//        }
-//
-//        userService.saveUser(userDto);
-//        return "redirect:/registration?success";
-//    }
+    @GetMapping("/registration")
+    public String registrationForm(Model model) {
+        UserDto user = new UserDto();
+        model.addAttribute("user", user);
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String registration(
+            @Valid @ModelAttribute("user") UserDto userDto,
+            BindingResult result,
+            Model model) {
+        User existingUser = userService.findUserByEmail(userDto.getEmail());
+
+        if (existingUser != null) {
+            result.rejectValue("email", null, "User already registered !!!");
+        }
+
+        if (result.hasErrors()) {
+            model.addAttribute("user", userDto);
+            return "registration";
+        }
+
+        userService.saveUser(userDto);
+        return "redirect:/registration?success";
+    }
 
     @GetMapping("/logout")
-    public String userLogout(){return "redirect:/login?logout";}
+    public String userLogout() {
+        return "redirect:/login?logout";
+    }
 }
